301a4bd6ad4ba3f91fb6214ee3cb8eb8cf87251c
Payalbh20 <117173906+Payalbh20@users.noreply.github.com>
2023-04-11 19:56:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 //your JS code here. If required.
-function Hello(){
-	return new Promise((resolve,reject)=>{
-		setTimeout(()=>{
-			resolve("Hello, world!");
-		},1000);
-	});
+
+function getMessage() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Hello, world!");
+    }, 1000);
+  });
 }
-Hello().then(data=>{
-   document.getElementById("output").textContent=data;
+
+getMessage().then((message) => {
+  document.getElementById("output").textContent = message;
 });
